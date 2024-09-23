a983ad1da6350a82210d868898d566b755e55575
simokhin <nick.simokhin@yandex.ru>
2024-09-23 15:20:41 +0300

    Add a function to change the grid size.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let container = document.querySelector("#container");
-
+let sizeButton = document.querySelector("#gridSizeButton");
 
 
 createSquare(16, 16);
@@ -8,11 +8,19 @@ function createSquare(sizeX, sizeY) {
     let sizeSquare = sizeX * sizeY;
     for (let i = 1; i <= sizeSquare; i++){
         let newDiv = document.createElement("div");
-        newDiv.setAttribute("id", "row");
+        newDiv.setAttribute("class", "row");
+        newDiv.setAttribute("style", `width: ${800 / sizeX}px; hight: ${800 / sizeX}px`);
         container.appendChild(newDiv);
         newDiv.addEventListener("mouseenter", (e) => {
-            newDiv.setAttribute("style", "background-color: #705C53")
+            newDiv.setAttribute("id", "rowColor")
         })
     }
 }
 
+sizeButton.addEventListener("click", () => {
+    let gridSize = prompt("Choose grid size");
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    createSquare(gridSize, gridSize);
+})
